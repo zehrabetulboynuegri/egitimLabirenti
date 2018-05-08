8426a08444dfa958d5ba3829a8347492fc63d199
UsaAbd <ausan15@posta.pau.edu.tr>
2018-05-08 14:53:40 +0300

    moduller birlestirildi

---
+++ Assets/Scripts/KarakterHareketi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterHareketi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-Input.GetAxis("Vertical")/20,0,0);
+		transform.Rotate (0,0,Input.GetAxis("Horizontal")*2);
+	}
+
+
+
+}
---
+++ Assets/Scripts/SoruGelme.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoruGelme : MonoBehaviour {
+
+	public GameObject panel;
+	public GameObject küp;
+
+	// Use this for initialization
+	public void Start () { 
+			panel.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag=="hedef") 
+		{
+			panel.gameObject.SetActive (true);
+			Destroy (küp);
+		}
+	}
+}
+	
